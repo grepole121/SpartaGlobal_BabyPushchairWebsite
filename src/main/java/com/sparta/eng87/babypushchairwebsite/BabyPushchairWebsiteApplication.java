ad58f59358bb0af9e117c01e7ba92590d84109c9
George Repole <grepole121@users.noreply.github.com>
2021-06-30 15:20:46 +0100

    Initial commit

---
+++ src/main/java/com/sparta/eng87/babypushchairwebsite/BabyPushchairWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.eng87.babypushchairwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyPushchairWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyPushchairWebsiteApplication.class, args);
+    }
+
+}
