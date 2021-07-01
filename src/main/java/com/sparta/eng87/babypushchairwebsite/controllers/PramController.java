54286536129bcdf046b832d3ca55c0bb686e63ea
AlexBL <alexbarberlynch@gmail.com>
2021-07-01 15:26:15 +0100

    fixed errors

--- src/main/java/com/sparta/eng87/babypushchairwebsite/controllers/PramController.java
+++ src/main/java/com/sparta/eng87/babypushchairwebsite/controllers/PramController.java
@@ -2,6 +2,7 @@ package com.sparta.eng87.babypushchairwebsite.controllers;
 
 import com.sparta.eng87.babypushchairwebsite.entities.PramEntity;
 import com.sparta.eng87.babypushchairwebsite.services.PramService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+
 @Controller
 public class PramController {
 
     private PramService pramService;
+
+    @Autowired
+    public PramController(PramService pramService) {
+        this.pramService = pramService;
+    }
 
     @GetMapping("/")
     public String getIndex() {
@@ -52,8 +60,8 @@ public class PramController {
 
     @GetMapping("/productPage/{id}")
     public String getProduct(@PathVariable("id") Integer id, Model model) {
-        model.addAttribute("product", pramService.findPramById(id));
-        return "productpage/{id}";
+        model.addAttribute("pram", pramService.findPramById(id));
+        return "product";
     }
 
     @PostMapping("/results")
@@ -62,14 +70,15 @@ public class PramController {
                            @RequestParam(name = "travel") String travel,
                            @RequestParam(name = "budget") String budget,
                            @RequestParam(name = "infants") String infants,
-                           @RequestParam(name = "accessories") String[] accessories,
-                           @RequestParam(name = "focalPoints") String[] focalPoints,
+                           @RequestParam(name = "accessories[]") String[] accessories,
+                           @RequestParam(name = "focalPoints[]") String[] focalPoints,
                            @RequestParam(name = "intTravel") String intTravel,
                            @RequestParam(name = "activity") String activity,
                            Model model) {
 
+        System.out.println(location + storage + Arrays.toString(accessories));
         model.addAttribute("prams", pramService.findPramsThatMeetCriteria(location, storage, travel, budget, infants, accessories, focalPoints, intTravel, activity));
-        return "/results";
+        return "results";
     }
 
 
