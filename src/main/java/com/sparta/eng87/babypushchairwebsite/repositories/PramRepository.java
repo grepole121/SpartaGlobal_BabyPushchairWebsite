92904352ea0c1168b5bdc551481a8f00b707231b
George Repole <grepole121@users.noreply.github.com>
2021-07-02 06:35:03 +0100

    Removed unnecessary if statements to clean up SQL query

--- src/main/java/com/sparta/eng87/babypushchairwebsite/repositories/PramRepository.java
+++ src/main/java/com/sparta/eng87/babypushchairwebsite/repositories/PramRepository.java
@@ -13,21 +13,21 @@ public interface PramRepository extends JpaRepository<PramEntity, Integer>{
 //    ReversableSeatFlag, , ,, AdjustableHandleFlag  --------- Not asked in questionnaire?
 
     @Query(value = "SELECT p.* FROM pramstable p WHERE p.discontinued_flag!=1 " +
-            "AND 'TRUE' = (IF(p.multi_terrain_flag OR NOT ?1, 'TRUE', 'FALSE'))" +
-            "AND 'TRUE' = (IF(p.compact_flag OR NOT ?2, 'TRUE', 'FALSE')) " +
-            "AND 'TRUE' = (IF(p.Car_Adapter_Flag OR NOT ?3, 'TRUE', 'FALSE')) " +
+            "AND (p.multi_terrain_flag OR NOT ?1) " +
+            "AND (p.compact_flag OR NOT ?2) " +
+            "AND (p.Car_Adapter_Flag OR NOT ?3) " +
             "AND (p.Cheapest_Price BETWEEN ?4 AND ?5) " +
             "AND (p.Child_Capacity BETWEEN ?6 AND ?7) " +
-            "AND 'TRUE' = (IF(p.Raincover_Flag OR NOT ?8, 'TRUE', 'FALSE')) " +
-            "AND 'TRUE' = (IF(p.Mosquitonet_Flag OR NOT ?9, 'TRUE', 'FALSE')) " +
-            "AND 'TRUE' = (IF(p.Parasol_Flag OR NOT ?10, 'TRUE', 'FALSE')) " +
-            "AND 'TRUE' = (IF(p.Changebag_Flag OR NOT ?11, 'TRUE', 'FALSE')) " +
+            "AND (p.Raincover_Flag OR NOT ?8) " +
+            "AND (p.Mosquitonet_Flag OR NOT ?9) " +
+            "AND (p.Parasol_Flag OR NOT ?10) " +
+            "AND (p.Changebag_Flag OR NOT ?11) " +
             "AND (p.Age_From_Months<=?12) " +
             "AND (p.Age_To_Months>=?13) " +
-            "AND 'TRUE' = (IF(p.Comfort_Flag OR NOT ?14, 'TRUE', 'FALSE')) " +
-            "AND 'TRUE' = (IF(p.Sturdy_Flag OR NOT ?15, 'TRUE', 'FALSE')) " +
-            "AND 'TRUE' = (IF(p.Ease_Of_Setup OR NOT ?16, 'TRUE', 'FALSE')) " +
-            "AND 'TRUE' = (IF(p.Large_Pram_Basket_Flag OR NOT ?17, 'TRUE', 'FALSE')) " +
+            "AND (p.Comfort_Flag OR NOT ?14) " +
+            "AND (p.Sturdy_Flag OR NOT ?15) " +
+            "AND (p.Ease_Of_Setup OR NOT ?16) " +
+            "AND (p.Large_Pram_Basket_Flag OR NOT ?17) " +
             "AND p.Weight < ?18"
             , nativeQuery = true)
     List<PramEntity> findPramsThatMeetCriteria(int MultiTerrainFlag,//Where do you live? & Activity BIT
